bbcbff3772302883085b72c17c450482f5f8ef8a
nmonterroso <nelsonmonterroso@gmail.com>
2015-12-16 15:59:24 -0800

    first pass at generated server

---
+++ generated/cmd/pacowsay-server/configure_pacowsay.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-swagger/go-swagger/errors"
+	"github.com/go-swagger/go-swagger/httpkit"
+	"github.com/go-swagger/go-swagger/httpkit/middleware"
+
+	"github.com/nmonterroso/pacowsay/generated/restapi/operations"
+)
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+func configureAPI(api *operations.PacowsayAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	api.JSONConsumer = httpkit.JSONConsumer()
+
+	api.JSONProducer = httpkit.JSONProducer()
+
+	api.CowsayHandler = operations.CowsayHandlerFunc(func(params operations.CowsayParams) middleware.Responder {
+		return middleware.NotImplemented("operation cowssay has not yet been implemented")
+	})
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
---
+++ generated/cmd/pacowsay-server/doc.go
@@ -0,0 +1,22 @@
+/*Package main pacowsay API
+
+functionality for a pacowsay server
+
+
+    Schemes:
+      http
+    Host: api.pacowsay.com
+    BasePath: /
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package main
